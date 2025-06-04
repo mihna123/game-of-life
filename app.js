ac0bbca56ccde6773a4e855e451df766bcf5b1f2
mihna123 <mihailonvojinovic@gmail.com>
2025-06-04 11:19:14 +0200

    Finishing touches

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const COLUMNS = 15;
-const ROWS = 15;
+const COLUMNS = 100;
+const ROWS = 100;
 
 /** @type {HTMLElement} */
 const gameSection = document.querySelector("section.game");
+
+let playEv = undefined;
 
 // Filling the game section with cells
 for (let i = 0; i < ROWS; ++i) {
@@ -25,12 +27,56 @@ for (const cell of cells) {
 }
 
 function tick() {
+	const toDie = [];
+	const toLive = [];
 	for (let i = 0; i < ROWS; ++i) {
 		for (let j = 0; j < COLUMNS; ++j) {
 			const row = gameSection.children.item(i);
 			const cell = row.children.item(j);
+			let neighbours = 0;
 
-			// TODO: logic to see if cell dies or thrives
+			for (let x = -1; x < 2; ++x) {
+				for (let y = -1; y < 2; ++y) {
+					const xind = i + x;
+					const yind = j + y;
+					if (xind < 0 || yind < 0 || xind >= ROWS || yind >= COLUMNS) {
+						continue;
+					}
+
+					const ncell = gameSection.children.item(xind).children.item(yind);
+					if (cell.isSameNode(ncell)) {
+						continue;
+					}
+					if (ncell.classList.contains("cell-on")) {
+						++neighbours;
+					}
+				}
+			}
+
+			if (neighbours < 2 || neighbours > 3) {
+				toDie.push(cell);
+			}
+			if (neighbours === 3) {
+				toLive.push(cell);
+			}
 		}
 	}
+	for (const c of toLive) {
+		c.classList.add("cell-on");
+	}
+
+	for (const c of toDie) {
+		c.classList.remove("cell-on");
+	}
 }
+
+document.querySelector("button.tick-btn").onclick = tick;
+document.querySelector("button.play-btn").onclick = () => {
+	if (playEv) return;
+	playEv = setInterval(tick, 100);
+};
+
+document.querySelector("button.stop-btn").onclick = () => {
+	clearInterval(playEv);
+	playEv = undefined;
+};
